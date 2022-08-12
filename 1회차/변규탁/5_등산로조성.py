6a4d1be3b290714e775c38298912a74cbd714c32
kleenex1 <curian4881@gmail.com>
2022-08-12 17:18:08 +0900

    모의고사

--- 1회차/변규탁/5_등산로조성.py
+++ 1회차/변규탁/5_등산로조성.py
@@ -1,35 +1,32 @@
 import sys
-from this import d
 
 sys.stdin = open("_등산로조성.txt")
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def dfs(i, j, cut = 0):
+def dfs(i, j, cnt, cut):
+    global answer
     mountain[i][j] = 1
-    stack = [(i, j)]
-    cnt = 0
-    while stack:
-        i, j = stack.pop()
-        cnt += 1
-        for d in range(4):
-            nx = i + dx[d]
-            ny = j + dy[d]
-            if 0 <= nx < N and 0 <= ny < N:
-                if mountain[nx][ny] == 0:
-                    if maps[i][j] <= maps[nx][ny]:
-                        if maps[i][j] > maps[nx][ny] - K:
-                            mountain[nx][ny] = 1
-                            dfs(nx,ny,cut)
-                            cut += 1
-                            stack.append((nx,ny))
-                    elif maps[i][j] > maps[nx][ny]:
-                        mountain[nx][ny] = 1
-                        stack.append((nx, ny))
+    for d in range(4):
+        nx = i + dx[d]
+        ny = j + dy[d]
+        if 0 <= nx < N and 0 <= ny < N:
+            if mountain[nx][ny] == 0:
+                if maps[i][j] > maps[nx][ny]:
+                    mountain[nx][ny] = 1
+                    dfs(nx, ny, cnt+1, cut)
+                    mountain[nx][ny] = 0
+                if cut == 0 and maps[nx][ny] >= maps[i][j] and maps[nx][ny] - maps[i][j] + 1 <= K:
+                    mountain[nx][ny] = 1
+                    height = maps[nx][ny]
+                    maps[nx][ny] = maps[nx][ny] - (maps[nx][ny]-maps[i][j] +1)
+                    dfs(nx, ny, cnt+1, 1)
+                    mountain[nx][ny] = 0
+                    maps[nx][ny] = height
 
-    return cnt, cut
-
+    answer = max(answer, cnt)
+    mountain[i][j] = 0
 
 T = int(input())
 
@@ -39,19 +36,14 @@ for test_case in range(1, T + 1):
     mountain = [[0]*N for _ in range(N)]
 
     top = 0
-   
-    for i in range(N):
-        top = max(maps[i])
-
-    max_ = 0
     for i in range(N):
         for j in range(N):
-            if maps[i][j] >= top:
+            if maps[i][j] > top:
                 top = maps[i][j]
-                cnt, cut = dfs(i,j)
-                if cut == 1 :
-                    max_ = max(cnt, dfs(i,j)[0]) 
+    answer = 0
+    for i in range(N):
+        for j in range(N):
+            if maps[i][j] == top:
+                dfs(i,j,1,0)
     
-
-    print(f'#{test_case} {max_}')
-   
+    print(f'#{test_case} {answer}')
